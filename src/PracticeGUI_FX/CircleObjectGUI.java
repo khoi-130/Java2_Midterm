7c7d74c93935e20a6b40f447acd97c3465b7b1dd
KhoiA <ngokho@sheridancollege.ca>
2022-03-09 22:06:13 -0500

    test exception on circle GUI

---
+++ src/PracticeGUI_FX/CircleObjectGUI.java
@@ -0,0 +1,69 @@
+package PracticeGUI_FX;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
+public class CircleObjectGUI extends Application {
+    
+    TextField txt1,txt2;
+    Button btn1;
+    Label lbl1,lbl2;
+    
+    public void start(Stage primaryStage) {
+        try {
+        GridPane pane = new GridPane();
+        btn1 = new Button("go");
+        lbl1 = new Label("Radius");
+        lbl2 = new Label("Area");
+        txt1 = new TextField("");
+        txt2 = new TextField("");
+        
+        pane.add(lbl1, 0, 0);        
+        pane.add(lbl2, 0, 1);
+        pane.add(txt1, 1, 0);
+        pane.add(txt2, 1, 1);
+        pane.add(btn1, 0, 2);
+ 
+        btn1.setOnAction(e -> go());
+        
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        } catch(Exception e) {
+            System.out.println("Error occured: " + e);
+        }
+        }
+
+    public void go() {
+        try {
+        CircleGUI circle = new CircleGUI();
+        circle.setRadius(getRadius());
+        setArea(circle.getArea());
+        } catch (Exception e) {
+            System.out.println("Error calculating area");
+        }
+        
+    }
+    
+    public double getRadius() {
+        try {
+        return Double.parseDouble(txt1.getText());
+        } catch (Exception e) {
+            System.out.println("Your input is invalid");
+        }
+        return 0;
+    }
+    
+    public void setArea(double value) {
+        txt2.setText(value + "");
+    }
+            
+    public static void main(String[] args ) {
+        launch(args);
+    }
+    
+}
